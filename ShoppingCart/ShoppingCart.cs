b99059c522a7fbf3f4dc2921eb67e33b060185ad
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-11-04 20:43:17 -0400

    move ShoppingCartItem to ShoppingCartItem.cs

--- ShoppingCart/ShoppingCart.cs
+++ ShoppingCart/ShoppingCart.cs
@@ -25,18 +25,5 @@ namespace ShoppingCart.ShoppingCart
 
     }
 
-    public record ShoppingCartItem(
-        int ProductCatalogueId,
-        string ProductName,
-        string Description,
-        Money Price)
-    {
-        public virtual bool Equals(ShoppingCartItem? obj) =>
-            obj != null && this.ProductCatalogueId.Equals(obj.ProductCatalogueId);
-
-        public override int GetHashCode() =>
-            this.ProductCatalogueId.GetHashCode();
-    }
-
     public record Money(string Currency, decimal Amount);
 }
