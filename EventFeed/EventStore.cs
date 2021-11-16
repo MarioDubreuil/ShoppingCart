de48ccfc96a1a98a05f54673dbfe68580583278e
Mario Dubreuil <mario_dubreuil@hotmail.com>
2021-11-15 20:08:15 -0500

    get events

--- EventFeed/EventStore.cs
+++ EventFeed/EventStore.cs
@@ -1,19 +1,24 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ShoppingCart.EventFeed
 {
     public class EventStore : IEventStore
     {
+        private static readonly List<Event> events = new();
         public IEnumerable<Event> GetEvents(long firstEventSequenceNumber, long lastEventSequenceNumber)
         {
-            throw new System.NotImplementedException();
+            return events;
+                    // .Where(e => e.SequenceNumber >= firstEventSequenceNumber && e.SequenceNumber <= lastEventSequenceNumber)
+                    // .OrderBy(e => e.SequenceNumber);
         }
 
         public void Raise(string eventName, object content)
         {
-            // throw new System.NotImplementedException();
-            Console.WriteLine($"An event has been raised: Event = {eventName} Object = {content}");
+            Console.WriteLine("An event has been raised");
+            events.Add(new Event(events.Count + 1, DateTimeOffset.UtcNow, eventName, content));
+            Console.WriteLine(events.Last());
         }
     }
 }
